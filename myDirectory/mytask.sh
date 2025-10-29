a9c56f3831cd34afa17466f4e43102922c036e25
ayman abohamed <abohamedayman22@gmail.com>
2025-10-29 15:12:42 +0300

    edit last Exercie

--- myDirectory/mytask.sh
+++ myDirectory/mytask.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+ayman@ayman-Lat5:~$ mkdir myDirectory
+ayman@ayman-Lat5:~$ cd myDirectory/
+ayman@ayman-Lat5:~/myDirectory$ mkdir secondDirectory
+ayman@ayman-Lat5:~/myDirectory$ cd secondDirectory/
+ayman@ayman-Lat5:~/myDirectory/secondDirectory$ touch myNotePaper
+ayman@ayman-Lat5:~/myDirectory/secondDirectory$ cp ~/myDirectory/secondDirectory/myNotePaper ~/myDirectory/
+ayman@ayman-Lat5:~/myDirectory/secondDirectory$ cd ..
+ayman@ayman-Lat5:~/myDirectory$ ls
+myNotePaper  secondDirectory
+ayman@ayman-Lat5:~/myDirectory$ mv ~/myDirectory/myNotePaper ~/myDirectory/myOldNotePaper
+ayman@ayman-Lat5:~/myDirectory$ ls
+myOldNotePaper  secondDirectory
 
 
